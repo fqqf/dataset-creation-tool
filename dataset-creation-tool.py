d21b2a2127421c416243168d077372886fedcc55
Mifia <xzx._@outlook.com>
2023-10-17 16:43:48 +0300

    Add ability to move frames within one id

--- dataset-creation-tool.py
+++ dataset-creation-tool.py
@@ -3,12 +3,14 @@ from tkinter import Label, PhotoImage
 import pandas as pd
 from PIL import Image, ImageTk, ImageDraw, ImageFont
 
-
-
 class App:
     def handle_keypress(self, event):
         if event.keysym == 'f':
             self.skip_gender()
+        elif event.keysym == 'Right':
+            self.next_image_same_id()
+        elif event.keysym == 'Left':
+            self.prev_image_same_id()
 
     def __init__(self, root, labels_file, image_dir):
         
@@ -17,7 +19,7 @@ class App:
         self.df.sort_values(by=['frame_num', 'id'], inplace=True)
         self.image_dir = image_dir
         
-        self.current_index = 337
+        self.current_index = 550
         self.processed_ids = (set(range(1,self.current_index)))
         # Load the first image
         self.load_image()
@@ -30,7 +32,7 @@ class App:
         self.canvas.pack()
         self.canvas.focus_set()
         self.canvas.bind("<Button-1>", self.on_click)
-        self.canvas.bind("<Key-f>", self.handle_keypress)
+        self.canvas.bind_all("<Key>", self.handle_keypress)
         self.update_image()
 
     def load_image(self):
@@ -53,15 +55,6 @@ class App:
         self.fill_gender(self.current_id(), 2)
         self.next_image()
 
-    def on_click(self, event):
-        if event.x > self.img_width // 2:
-            self.fill_gender(self.current_id(), 0)
-        else:
-            self.fill_gender(self.current_id(), 1)
-        
-        self.next_image()
-        
-
     def fill_gender(self, id_value, side):
         self.last_side = side
         print(id_value)
@@ -70,7 +63,6 @@ class App:
         if side==2: return
         self.df.loc[self.df['id'] == id_value, 'gender'] = int(side)
         self.df.to_csv(f'./labels.csv',index=False)
-
 
     def current_id(self):
         return self.df.iloc[self.current_index]['id']
@@ -87,18 +79,46 @@ class App:
         self.load_image()
         self.update_image()
 
+    def next_image_same_id(self):
+        current_id_val = self.current_id()
+        
+        # Store the starting index as a safeguard
+        starting_index = self.current_index
+
+        while self.current_index < len(self.df) - 1:
+            self.current_index += 1
+            if self.df.iloc[self.current_index]['id'] == current_id_val:
+                break
+
+        # If the next image's id doesn't match the current id, revert back to the starting index
+        if self.df.iloc[self.current_index]['id'] != current_id_val:
+            self.current_index = starting_index
+
+        self.load_image()
+        self.update_image()
+
+    def prev_image_same_id(self):
+        current_id_val = self.current_id()
+        
+        # Store the starting index as a safeguard
+        starting_index = self.current_index
+
+        while self.current_index > 0:
+            self.current_index -= 1
+            if self.df.iloc[self.current_index]['id'] == current_id_val:
+                break
+
+        # If the previous image's id doesn't match the current id, revert back to the starting index
+        if self.df.iloc[self.current_index]['id'] != current_id_val:
+            self.current_index = starting_index
+
+        self.load_image()
+        self.update_image()
+
+
     def update_image(self):
         self.canvas.create_image(0, 0, anchor="nw", image=self.image_obj)
         self.label_id.config(text=f"ID: {int(self.current_id())}/5965")
-
-    # Загрузка файла txt в DataFrame
-    def load_data(file_path):
-        df = pd.read_csv(file_path, sep=',')
-        df['gender'] = np.nan  # Создание пустой колонки 'gender'
-        return df
-
-
-
 
 if __name__ == "__main__":
     root = tk.Tk()
